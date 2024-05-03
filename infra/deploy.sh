f3f296f3449df1af74b93707450db7444039caec
Smita Srivastava <smisriv@amazon.com>
2024-05-03 12:28:01 +0530

    Updates to fix ash outputs

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ux
 
-export EC_FS_STACK="elastic-cache-feature-store-stack"
+# 1) CONFIG
+# read the configuration file to load variables into this shell script
+source config
 
-echo $EC_FS_STACK
+export FEAST_BUCKET_NAME="${PROJECT_NAME}-bucket"
 
 # 2) 
 echo "Checking if the stack exist"
@@ -13,27 +15,34 @@ if ! aws cloudformation describe-stacks --stack-name $EC_FS_STACK  --region "us-
                 --template-body file://template.yaml \
                 --capabilities CAPABILITY_NAMED_IAM \
                 --region 'us-west-2' \
-                --parameters ParameterKey=pAdminPassword,ParameterValue="thisISyourPassword1"
+                --parameters \
+                        ParameterKey=pAdminPassword,ParameterValue="thisISyourPassword1" \
+                        ParameterKey=pProjectName,ParameterValue=$PROJECT_NAME \
+                        ParameterKey=pRedshiftSubnetGroup,ParameterValue=$SUBNET_ID
+
 else
         aws cloudformation update-stack \
                 --stack-name $EC_FS_STACK \
                 --template-body file://template.yaml \
                 --capabilities CAPABILITY_NAMED_IAM \
                 --region 'us-west-2' \
-                --parameters ParameterKey=pAdminPassword,ParameterValue="thisISyourPassword1"
+                --parameters \
+                        ParameterKey=pAdminPassword,ParameterValue="thisISyourPassword1" \
+                        ParameterKey=pProjectName,ParameterValue=$PROJECT_NAME \
+                        ParameterKey=pRedshiftSubnetGroup,ParameterValue=$SUBNET_ID
 fi
 
-sleep 10s
+sleep 60s
 
-aws s3api put-object --bucket "demo-feast-project-aws-test-bucket" \
+aws s3api put-object --bucket $FEAST_BUCKET_NAME \
         --key "zipcode_table/table.parquet" \
         --body "../data/zipcode_table.parquet"
 
 
-aws s3api put-object --bucket "demo-feast-project-aws-test-bucket" \
+aws s3api put-object --bucket $FEAST_BUCKET_NAME \
         --key "credit_history/table.parquet" \
         --body "../data/credit_history.parquet"
 
-aws s3api put-object --bucket "demo-feast-project-aws-test-bucket" \
+aws s3api put-object --bucket $FEAST_BUCKET_NAME \
         --key "loan_features/table.parquet" \
         --body "../data/loan_table.parquet"
